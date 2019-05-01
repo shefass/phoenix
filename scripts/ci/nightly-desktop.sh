5fec90dd86acafd97cc02c6128cc4caab989376f
ohager <oliver.hager@dextra-sw.com>
2019-05-01 17:59:40 -0300

    more on nightly

--- scripts/ci/nightly-desktop.sh
+++ scripts/ci/nightly-desktop.sh
@@ -4,26 +4,42 @@ echo ===============================
 echo Nightly Release Desktop Wallet
 echo ===============================
 
-npm i @angular/cli -g
+echo TEST SCRIPT!
 
-# install script deps
-echo
-cd ./scripts
-npm i
+if [ "$TRAVIS_BRANCH" != "nightly" ]; then
+    echo "This script works only on branch 'nightly'"
+    exit 0;
+fi
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch --all
+git checkout develop
+git pull
+git checkout nightly
+git merge --no-ff origin/develop
+git push @github.com/">https://${GH_TOKEN}@github.com/burst-apps-team/phoenix.git"
 
-# install/update version of burstjs
-cd ../lib
-npm install
-npm run bootstrap
 
-# install/update angular wallet deps
-cd ../web/angular-wallet
-npm i
 
-# install/update desktop wallet deps
-cd ../../desktop/wallet
-npm install
-
-# only builds, but not publish
-#npm run release:macos
-
+#npm i @angular/cli -g
+#
+## install script deps
+#echo
+#cd ./scripts
+#npm i
+#
+## install/update version of burstjs
+#cd ../lib
+#npm install
+#npm run bootstrap
+#
+## install/update angular wallet deps
+#cd ../web/angular-wallet
+#npm i
+#
+## install/update desktop wallet deps
+#cd ../../desktop/wallet
+#npm install
+#
+## only builds, but not publish
+##npm run release:macos
+#
